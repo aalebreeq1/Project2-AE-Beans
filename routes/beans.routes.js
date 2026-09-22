f830d0729a8ec9f1b4ace31be1552d4729456c99
aalebreeq1 <abdullaebreeq1@gmail.com>
2026-09-22 20:41:55 +0300

    add: create edit and update route for beans model

--- routes/beans.routes.js
+++ routes/beans.routes.js
@@ -6,9 +6,9 @@ const Bean = require("../models/Bean")
 
 router.get('/', async (req, res) => {
     try {
-        const  allBeans= await Bean.findAll({ where: { isDeleted: false } })
+        const allBeans = await Bean.findAll({ where: { isDeleted: false } })
         res.render('all-beans.ejs', { allBeans })
-        
+
     }
     catch (err) {
         console.error(err)
@@ -17,20 +17,59 @@ router.get('/', async (req, res) => {
 
 
 
-router.get('/create', isAdmin, isSignedIn, (req,res) => {
-    try{
+router.get('/create', isAdmin, isSignedIn, (req, res) => {
+    try {
         res.render('create-bean.ejs')
     }
-    catch (err){
+    catch (err) {
         console.error(err)
     }
 })
 
-// router.post('/', isAdmin, (req,res)=> {
-//     try{
-//     }
+router.post('/', isAdmin, isSignedIn, async (req, res) => {
+    try {
+        const { name, countryOfOrigin, notes, grading_score, price, roasting_date, img_url } = req.body
+        const newBean = await Bean.create({ name, countryOfOrigin, notes, grading_score, price, roasting_date, img_url })
+        res.redirect('/beans')
 
-// }  )
+
+    }
+    catch (err) {
+        console.error(err)
+    }
+
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const bean = await Bean.findById(req.params.id)
+        res.render('bean-details.ejs', { bean })
+    }
+    catch (err) {
+        console.error(err)
+    }
+})
+
+router.get('/:id/edit', isAdmin, isSignedIn, async (req, res) => {
+    try {
+        const beanToEdit = await Bean.findById(req.params.id)
+        res.render('edit-bean.ejs', { bean: beanToEdit })
+    }
+    catch (err) {
+        console.error(err)
+    }
+})
+
+router.put('/:id/update', isAdmin, isSignedIn, async (req, res) => {
+    try {
+        const { name, countryOfOrigin, notes, grading_score, price, roasting_date, img_url } = req.body
+        const beanToUpdate = await Bean.findByIdAndUpdate(req.params.id, { name, countryOfOrigin, notes, grading_score, price, roasting_date, img_url })
+        res.redirect('/beans/' + req.params.id)
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
 
 
 module.exports = router;
