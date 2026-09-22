00508db774c918563dc0c5eb6ee20ae2f078c320
aalebreeq1 <abdullaebreeq1@gmail.com>
2026-09-22 19:20:15 +0300

    feat: create coffe_tools model

---
+++ models/Coffee_tools.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const coffeeToolSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    type:{
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    img_url: {
+        type: String,
+        required: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true })
+
+const CoffeeTool = mongoose.model('CoffeeTool', coffeeToolSchema)
+module.exports = CoffeeTool
