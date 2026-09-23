e789c37a3360d0a2953aa0dfa7cc6726dca843dd
aalebreeq <abdullaebreeq1@gmail.com>
2026-09-23 19:53:23 +0300

    fix(schema): fix typo in shipping company schema variable name

--- models/Shipping_company.js
+++ models/Shipping_company.js
@@ -1,21 +1,25 @@
 const mongoose = require('mongoose')
 
-const shippingCompantSchema= new mongoose.Schema({
-    name:{
+const shippingCompanySchema = new mongoose.Schema({
+    name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     address: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     phone_number: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     img_url: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     isDeleted: {
         type: Boolean,
@@ -23,5 +27,5 @@ const shippingCompantSchema= new mongoose.Schema({
     }
 }, { timestamps: true })
 
-const ShippingCompany = mongoose.model('ShippingCompany', shippingCompantSchema)
+const ShippingCompany = mongoose.model('ShippingCompany', shippingCompanySchema)
 module.exports = ShippingCompany
