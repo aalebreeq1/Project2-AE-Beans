9f29cdf1519aa37abcd5dff97083fb41bfb7ecae
aalebreeq <abdullaebreeq1@gmail.com>
2026-09-23 20:11:00 +0300

    feat(routes): add coffee tool update route for form submission

--- routes/coffeetools.routes.js
+++ routes/coffeetools.routes.js
@@ -1,28 +1,28 @@
-const mongoose = require("mongoose");
-const router = require("express").Router();
-const CoffeeTool = require("../models/CoffeeTool.model.js");
-const isAdmin = require("../middleware/is-admin.js");
+const mongoose = require("mongoose")
+const router = require("express").Router()
+const CoffeeTool = require("../models/CoffeeTool.model.js")
+const isAdmin = require("../middleware/is-admin.js")
 
 router.get("/", async (req, res) => {
   try {
-    const coffeeTools = await CoffeeTool.find({ is_deleted: false });
-    res.render("tools/all-tools.ejs", { coffeeTools });
+    const coffeeTools = await CoffeeTool.find({ is_deleted: false })
+    res.render("tools/all-tools.ejs", { coffeeTools })
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
-});
+})
 
 router.get("/create", isAdmin, (req, res) => {
   try {
-    res.render("tools/create-tool.ejs");
+    res.render("tools/create-tool.ejs")
   } catch (err) {
-    console.error(err);
+    console.error(err)
   }
-});
+})
 
 router.post("/", isAdmin, async (req, res) => {
   try {
-    const { name, category, price, quantity, img_url } = req.body;
+    const { name, category, price, quantity, img_url } = req.body
 
     await CoffeeTool.create({
       name,
@@ -30,22 +30,40 @@ router.post("/", isAdmin, async (req, res) => {
       price,
       quantity,
       img_url,
-    });
+    })
 
-    res.redirect("/coffee-tools");
+    res.redirect("/coffee-tools")
   } catch (err) {
-    console.error(err);
-    res.redirect("/coffee-tools/create");
+    console.error(err)
+    res.redirect("/coffee-tools/create")
   }
-});
+})
 
 router.get("/:id/edit", isAdmin, async (req, res) => {
   try {
-    const coffeeToolToEdit = await CoffeeTool.findById(req.params.id);
-    res.render("tools/edit-tool.ejs", { coffeeTool: coffeeToolToEdit });
+    const coffeeToolToEdit = await CoffeeTool.findById(req.params.id)
+    res.render("tools/edit-tool.ejs", { coffeeTool: coffeeToolToEdit })
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-});
+})
 
-module.exports = router;
+router.post("/:id/update", isAdmin, async (req, res) => {
+  try {
+    const { name, category, price, quantity, img_url } = req.body
+    const coffeeToolToUpdate = await CoffeeTool.findByIdAndUpdate(
+      req.params.id,
+      {
+        name,
+        category,
+        price,
+        quantity,
+        img_url,
+      },
+    )
+    res.redirect("/coffee-tools")
+  } catch (err) {
+    console.log(err)
+  }
+})
+module.exports = router
