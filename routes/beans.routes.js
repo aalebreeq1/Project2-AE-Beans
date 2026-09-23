895d702c3748a303ecf3380bc8958885c84b0782
aalebreeq <abdullaebreeq1@gmail.com>
2026-09-23 19:57:48 +0300

    fix(routes): update bean routes for Mongoose and schema alignment
    - Replace Sequelize .findAll() with Mongoose .find()
    - Include quantity and description fields in create and update operations
    - Fix country_of_origin field naming and reorder middleware checks

--- routes/beans.routes.js
+++ routes/beans.routes.js
@@ -1,85 +1,119 @@
 const router = require("express").Router()
 const isAdmin = require("../middleware/is-admin")
-const isSignedIn = require("../middleware/is-signed-in")
 const Bean = require("../models/Bean")
 
-
-router.get('/', async (req, res) => {
-    try {
-        const allBeans = await Bean.findAll({ where: { isDeleted: false } })
-        res.render('all-beans.ejs', { allBeans })
-
-    }
-    catch (err) {
-        console.error(err)
-    }
+router.get("/", async (req, res) => {
+  try {
+    const allBeans = await Bean.find({ isDeleted: false })
+    res.render("all-beans.ejs", { allBeans })
+  } catch (err) {
+    console.error(err)
+    res.redirect("/")
+  }
 })
 
-
-
-router.get('/create', isAdmin, isSignedIn, (req, res) => {
-    try {
-        res.render('create-bean.ejs')
-    }
-    catch (err) {
-        console.error(err)
-    }
+router.get("/create", isAdmin, (req, res) => {
+  try {
+    res.render("create-bean.ejs")
+  } catch (err) {
+    console.error(err)
+  }
 })
 
-router.post('/', isAdmin, isSignedIn, async (req, res) => {
-    try {
-        const { name, countryOfOrigin, notes, grading_score, price, roasting_date, img_url } = req.body
-        const newBean = await Bean.create({ name, countryOfOrigin, notes, grading_score, price, roasting_date, img_url })
-        res.redirect('/beans')
+router.post("/", isAdmin, async (req, res) => {
+  try {
+    const {
+      name,
+      country_of_origin,
+      notes,
+      description,
+      grading_score,
+      price,
+      roasting_date,
+      quantity,
+      img_url,
+    } = req.body
 
+    await Bean.create({
+      name,
+      country_of_origin,
+      notes,
+      description,
+      grading_score,
+      price,
+      roasting_date,
+      quantity,
+      img_url,
+    })
 
-    }
-    catch (err) {
-        console.error(err)
-    }
-
+    res.redirect("/beans")
+  } catch (err) {
+    console.error(err)
+    res.redirect("/beans/create")
+  }
 })
 
-router.get('/:id', async (req, res) => {
-    try {
-        const bean = await Bean.findById(req.params.id)
-        res.render('bean-details.ejs', { bean })
-    }
-    catch (err) {
-        console.error(err)
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const bean = await Bean.findById(req.params.id)
+    res.render("bean-details.ejs", { bean })
+  } catch (err) {
+    console.error(err)
+    res.redirect("/beans")
+  }
 })
 
-router.get('/:id/edit', isAdmin, isSignedIn, async (req, res) => {
-    try {
-        const beanToEdit = await Bean.findById(req.params.id)
-        res.render('edit-bean.ejs', { bean: beanToEdit })
-    }
-    catch (err) {
-        console.error(err)
-    }
+router.get("/:id/edit", isAdmin, async (req, res) => {
+  try {
+    const beanToEdit = await Bean.findById(req.params.id)
+    res.render("edit-bean.ejs", { bean: beanToEdit })
+  } catch (err) {
+    console.error(err)
+    res.redirect("/beans")
+  }
 })
 
-router.put('/:id/update', isAdmin, isSignedIn, async (req, res) => {
-    try {
-        const { name, countryOfOrigin, notes, grading_score, price, roasting_date, img_url } = req.body
-        const beanToUpdate = await Bean.findByIdAndUpdate(req.params.id, { name, countryOfOrigin, notes, grading_score, price, roasting_date, img_url })
-        res.redirect('/beans/' + req.params.id)
-    }
-    catch (err) {
-        console.log(err)
-    }
+router.put("/:id/update", isAdmin, async (req, res) => {
+  try {
+    const {
+      name,
+      country_of_origin,
+      notes,
+      description,
+      grading_score,
+      price,
+      roasting_date,
+      quantity,
+      img_url,
+    } = req.body
+
+    await Bean.findByIdAndUpdate(req.params.id, {
+      name,
+      country_of_origin,
+      notes,
+      description,
+      grading_score,
+      price,
+      roasting_date,
+      quantity,
+      img_url,
+    })
+
+    res.redirect("/beans/" + req.params.id)
+  } catch (err) {
+    console.log(err)
+    res.redirect("/beans/" + req.params.id + "/edit")
+  }
 })
 
-router.delete('/:id/delete', isAdmin, isSignedIn, async (req, res) => {
-    try {
-        const beanToDelete = await Bean.findByIdAndUpdate(req.params.id, { isDeleted: true })
-        res.redirect('/beans')
-    }
-    catch (err) {
-        console.error(err)
-    }
+router.delete("/:id/delete", isAdmin, async (req, res) => {
+  try {
+    await Bean.findByIdAndUpdate(req.params.id, { isDeleted: true })
+    res.redirect("/beans")
+  } catch (err) {
+    console.error(err)
+    res.redirect("/beans")
+  }
 })
 
-
-module.exports = router;
+module.exports = router
