7910a6113162149da94764ce63dda06cd97d5d42
aalebreeq <abdullaebreeq1@gmail.com>
2026-09-23 19:32:52 +0300

    refactor: add description field to coffeeTool model

--- models/Beans.js
+++ models/Beans.js
@@ -1,51 +1,56 @@
-const mongoose =require('mongoose')
+const mongoose = require("mongoose");
 
-const beanSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-        trim: true
+const beanSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    country_of_origin:{
-        type: String,
-        required: true,
-        trim: true
+    country_of_origin: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    notes:{
-        type: String,
-        trim: true,
+    notes: {
+      type: String,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    grading_score: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 100,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    roasting_date: {
+      type: Date,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    img_url: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
 
-    },
-    grading_score:{
-        type: Number,
-        required: true,
-        min: 0,
-        max: 100
-    },
-    price:{
-        type: Number,
-        required: true,
-        min: 0
-    },
-    roasting_date:{
-        type: Date,
-        required: true
-    },
-    quantity:{
-        type: Number,
-        required: true,
-        min: 0
-    },
-    img_url:{
-        type: String,
-        trim: true,
-        required: true
-    },
-    isDeleted:{
-        type: Boolean,
-        default: false
-    }
-},{timestamps: true})
-
-const Bean = mongoose.model('Bean', beanSchema)
-module.exports = Bean
+const Bean = mongoose.model("Bean", beanSchema);
+module.exports = Bean;
--- routes/coffeetools.routes.js
+++ routes/coffeetools.routes.js
@@ -4,20 +4,27 @@ const CoffeeTool = require("../models/CoffeeTool.model.js")
 const isAdmin = require("../middleware/is-admin.js")
 
 router.get("/", async (req, res) => {
-    try {
-        const coffeeTools = await CoffeeTool.find({ is_deleted: false })
-        res.render("tools/all-tools.ejs", { coffeeTools })
-    }
-    catch (err) {
-        console.error(err)
-    }
+  try {
+    const coffeeTools = await CoffeeTool.find({ is_deleted: false })
+    res.render("tools/all-tools.ejs", { coffeeTools })
+  } catch (err) {
+    console.error(err)
+  }
 })
 
-router.get("/create" , isAdmin, (req,res) =>{
+router.get("/create", isAdmin, (req, res) => {
+  try {
+    res.render("tools/create-tool.ejs")
+  } catch (err) {
+    console.error(err)
+  }
+})
+
+router.post("/" ,isAdmin, async (req, res) =>{
     try{
-        res.render("tools/create-tool.ejs")
+        // const {name, type , price, des}
     }
-    catch (err) { 
+    catch (err){
         console.error(err)
     }
 })
