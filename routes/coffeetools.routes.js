e8a09ed86df01d7272f7f2933458426b937afc23
aalebreeq <abdullaebreeq1@gmail.com>
2026-09-23 20:00:12 +0300

    feat(routes): add coffee tool creation route

--- routes/coffeetools.routes.js
+++ routes/coffeetools.routes.js
@@ -1,31 +1,42 @@
-const mongoose = require("mongoose")
-const router = require("express").Router()
-const CoffeeTool = require("../models/CoffeeTool.model.js")
-const isAdmin = require("../middleware/is-admin.js")
+const mongoose = require("mongoose");
+const router = require("express").Router();
+const CoffeeTool = require("../models/CoffeeTool.model.js");
+const isAdmin = require("../middleware/is-admin.js");
 
 router.get("/", async (req, res) => {
   try {
-    const coffeeTools = await CoffeeTool.find({ is_deleted: false })
-    res.render("tools/all-tools.ejs", { coffeeTools })
+    const coffeeTools = await CoffeeTool.find({ is_deleted: false });
+    res.render("tools/all-tools.ejs", { coffeeTools });
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-})
+});
 
 router.get("/create", isAdmin, (req, res) => {
   try {
-    res.render("tools/create-tool.ejs")
+    res.render("tools/create-tool.ejs");
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-})
+});
 
-router.post("/" ,isAdmin, async (req, res) =>{
-    try{
-        // const {name, type , price, des}
-    }
-    catch (err){
-        console.error(err)
-    }
-})
-module.exports = router
+router.post("/", isAdmin, async (req, res) => {
+  try {
+    const { name, category, price, quantity, img_url } = req.body;
+
+    await CoffeeTool.create({
+      name,
+      category,
+      price,
+      quantity,
+      img_url,
+    });
+
+    res.redirect("/coffee-tools");
+  } catch (err) {
+    console.error(err);
+    res.redirect("/coffee-tools/create");
+  }
+});
+
+module.exports = router;
