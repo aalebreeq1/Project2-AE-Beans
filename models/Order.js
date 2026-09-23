750f23b250ec40ad5dd4d051e0749ab97fcd9d5a
aalebreeq <abdullaebreeq1@gmail.com>
2026-09-23 19:40:14 +0300

    refactor: add shipping company field to order model

--- models/Order.js
+++ models/Order.js
@@ -1,59 +1,66 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const itemSchema = new mongoose.Schema({
-    bean: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Bean',
-        required: true
-    },
-    beans_quantity: {
-        type: Number,
-        required: true,
-        min: 1
-    },
-    coffee_tools: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'CoffeeTool',
-        required: true
-    },
-    tools_quantity: {
-        type: Number,
-        required: true,
-        min: 1
-    }
+  bean: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Bean",
+    required: true,
+  },
+  beans_quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  coffee_tools: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "CoffeeTool",
+    required: true,
+  },
+  tools_quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
 })
-const orderSchema = new mongoose.Schema({
+const orderSchema = new mongoose.Schema(
+  {
     owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
-    items: [
-        itemSchema
-    ],
+    items: [itemSchema],
     isDeleted: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     total_price: {
-        type: Number,
-        required: true,
-        min: 0
+      type: Number,
+      required: true,
+      min: 0,
     },
     order_date: {
-        type: Date,
-        required: true,
-        default: Date.now
+      type: Date,
+      required: true,
+      default: Date.now,
     },
     shipping_company: {
-        type: String,
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ShippingCompany",
+      required: true,
+    },
+    shipping_address: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User.address",
+      required: true,
     },
     isDeleted: {
-        type: Boolean,
-        default: false
-    }
-}, { timestamps: true })
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+)
 
-const Order = mongoose.model('Order', orderSchema)
+const Order = mongoose.model("Order", orderSchema)
 module.exports = Order
