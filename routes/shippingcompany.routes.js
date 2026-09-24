4698bfe2507a2c3789e533a8c0adab4ae0a7b894
aalebreeq <abdullaebreeq1@gmail.com>
2026-09-24 21:14:59 +0300

    feat(route): create route that display all available shipping company

--- routes/shippingcompany.routes.js
+++ routes/shippingcompany.routes.js
@@ -3,4 +3,14 @@ const router = require('express').Router()
 const ShippingCompany = require('../models/Shipping_company')
 
 
+router.get('/', async (req,res)=> {
+    try{
+        const shippingCompanies = await ShippingCompany.find({ is_deleted: false})
+        res.render('shipping/all-shipping-companies.ejs',{ shippingCompanies })
+    }
+    catch (err){
+        console.log(err)
+    }
+})
+
 module.exports = router
