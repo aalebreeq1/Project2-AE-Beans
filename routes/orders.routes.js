50ad6f501b3f84631532f27e311805fe9b994c2e
aalebreeq <abdullaebreeq1@gmail.com>
2026-09-26 14:26:00 +0300

    feat(route): create'/:id/edit' route that display order edit form

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 const router = require("express").Router()
 const Order = require("../models/Order")
 const isSignedIn = require("../middlewares/auth")
+const isAdmin = require("../middlewares/admin")
 router.get("/", isSignedIn, async (req, res) => {
   try {
     const Orders = await Order.findById(req.session.user._id)
@@ -32,13 +33,23 @@ router.post("/", isSignedIn, async (req, res) => {
   }
 })
 
-router.get("/:id", isSignedIn, async (req, res) => {
+router.get("/:id/", isSignedIn, async (req, res) => {
   try {
     const order = await Order.findById(req.params.id)
     res.render("order-details", { order })
   } catch (err) {
     console.log(err)
     res.redirect("/all-orders")
+  }
+})
+
+router.post("/:id/edit", isAdmin, async (req, res) => {
+  try {
+    const foundOrder = await Order.findById(req.params.id)
+    res.render("edit-order", { order: foundOrder })
+  } catch (err) {
+    console.log(err)
+    res.redirect(`/orders/${req.params.id}/`)
   }
 })
 
